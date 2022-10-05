19a9a70565b0495a793a9af86052e53d2971346d
Holden Karau <holden@pigscanfly.ca>
2022-10-04 17:51:30 -0700

    Start renaming the SQL sub-package.

--- sql/setup.py
+++ sql/setup.py
@@ -2,8 +2,8 @@
 from setuptools import find_packages, setup
 
 # Change these names in your plugin, e.g. company name or plugin purpose.
-PLUGIN_LOGICAL_NAME = "example"
-PLUGIN_ROOT_MODULE = "example"
+PLUGIN_LOGICAL_NAME = "sparksql"
+PLUGIN_ROOT_MODULE = "sparksql"
 
 setup(
     name="sqlfluff-plugin-{plugin_logical_name}".format(
@@ -12,7 +12,7 @@ setup(
     include_package_data=True,
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires="sqlfluff>=0.4.0",
+    install_requires="sqlfluff>=1.0.0",
     entry_points={
         "sqlfluff": [
             "{plugin_logical_name} = {plugin_root_module}.rules".format(
