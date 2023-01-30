0dd3dd4451a26f07a44e195d39de24fe7398a17b
Holden Karau <holden@pigscanfly.ca>
2023-01-30 12:55:49 -0800

    WAP Plugin test runs (and then fails)

--- iceberg-spark-upgrade-wap-plugin/project/Dependencies.scala
+++ iceberg-spark-upgrade-wap-plugin/project/Dependencies.scala
@@ -5,6 +5,6 @@ object Dependencies {
   lazy val iceberg = "org.apache.iceberg" % "iceberg-core" % "0.9.1"
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.10"
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4"
-  lazy val sparkTestingBase = "com.holdenkarau" %% "spark-testing-base" % "3.2.2_1.3.1-SNAPSHOT"
+  lazy val sparkTestingBase = "com.holdenkarau" %% "spark-testing-base" % "3.2.2_1.3.3-SNAPSHOT"
   lazy val icebergSparkRuntime = "org.apache.iceberg" %% "iceberg-spark-runtime-3.2" % "1.1.0"
 }
--- iceberg-spark-upgrade-wap-plugin/src/test/scala/com/holdenkarau/spark/upgrade/wap/plugin/WAPIcebergSpec.scala
+++ iceberg-spark-upgrade-wap-plugin/src/test/scala/com/holdenkarau/spark/upgrade/wap/plugin/WAPIcebergSpec.scala
@@ -1,4 +1,4 @@
-package com.holdenkarau
+package com.holdenkarau.spark.upgrade.wap.plugin
 
 import java.io.File
 
@@ -8,21 +8,29 @@ import com.holdenkarau.spark.testing.Utils
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import org.apache.spark.SparkConf
+
 class WAPIcebergSpec extends AnyFunSuite with ScalaDataFrameSuiteBase with Matchers {
 
   override protected def enableHiveSupport = false
   override protected def enableIcebergSupport = true
 
-  // For now https://github.com/sbt/sbt/issues/7137 no test.
-  /**
-   * Ok testing this does not work with sbt for reasons that I'm a bit fuzzy on but it looks like there has been some classpath
-   * shenanigins as of late.
-   *
+  override def conf: SparkConf = {
+    new SparkConf().
+      setMaster("local[*]").
+      setAppName("test").
+      set("spark.ui.enabled", "false").
+      set("spark.app.id", appID).
+      set("spark.driver.host", "localhost").
+      // Hack
+      set("spark.driver.userClassPathFirst", "true").
+      set("spark.executor.userClassPathFirst", "true")
+  }
+
   test("WAPIcebergSpec should be called on iceberg commit") {
     val cl = Thread.currentThread.getContextClassLoader
     cl.loadClass("org.apache.iceberg.spark.SparkSessionCatalog")
     spark.sql("CREATE TABLE local.db.table (id bigint, data string) USING iceberg")
     WAPIcebergListener.lastLog shouldEqual "farts"
   }
-   */
 }
