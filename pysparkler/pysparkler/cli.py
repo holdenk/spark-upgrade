f645fb633fd997088fce4c1f8c0a8801f32ff9bd
Dhruv Pratap <dpratap@netflix.com>
2023-03-31 11:10:32 -0400

    PySparkler: Add dry-run mode and show unified diffs with Syntax highlighting for an easy validation mode.

--- pysparkler/pysparkler/cli.py
+++ pysparkler/pysparkler/cli.py
@@ -20,42 +20,95 @@ import difflib
 
 import click
 from click import Context
+from rich.console import Console
+from rich.syntax import Syntax
+from rich.table import Table
 
 from pysparkler.api import PySparkler
 
+stdout = Console()
+stderr = Console(stderr=True, style="bold red")
+
 
 @click.group()
-@click.option("-v", "--verbose", type=click.BOOL)
-@click.option("-d", "--dry-run", type=click.BOOL)
+@click.option("-v", "--verbose", is_flag=True, help="Verbose mode")
 @click.pass_context
-def run(ctx: Context, verbose: bool, dry_run: bool) -> None:
+def run(ctx: Context, verbose: bool) -> None:
     """Pysparkler CLI - A tool to upgrade PySpark scripts to newer versions"""
     ctx.ensure_object(dict)
     ctx.obj["verbose"] = verbose
-    ctx.obj["dry_run"] = dry_run
 
 
 @run.command()
-@click.option("-o", "--output-file", type=click.Path(exists=False, writable=True))
-@click.argument("input_file", type=click.Path(exists=True, readable=True))
-def upgrade(input_file: str, output_file: str | None) -> None:
+@click.option(
+    "-i",
+    "--input-file",
+    required=True,
+    type=click.Path(exists=True, readable=True),
+    help="The input file to upgrade. This is re-written in place if no output file is provided and not in dry-run mode",
+)
+@click.option(
+    "-o",
+    "--output-file",
+    type=click.Path(exists=False, writable=True),
+    help="Output file to be written against the input. This is ignored in dry-run mode.",
+)
+@click.option(
+    "-d",
+    "--dry-run",
+    is_flag=True,
+    help="Dry run mode, just shows a unified diff across input and output.",
+)
+@click.pass_context
+def upgrade(
+    ctx: Context,
+    input_file: str,
+    output_file: str | None,
+    dry_run: bool,
+) -> None:
     """Upgrade the PySparkler file to the latest version and provides comments as hints for manual changes"""
 
-    click.echo(f"Input File: {input_file}")
-    if output_file:
-        click.echo(f"Output File: {output_file}")
-
+    print_command_params(ctx)
     pysparkler = PySparkler()
-    output_file_content = pysparkler.upgrade(input_file)
-
-    with open(input_file, encoding="utf-8") as f:
-        input_file_content = f.read()
-
-    click.echo("Unified Diff:")
-    click.echo(
-        "".join(
-            difflib.unified_diff(
-                input_file_content.splitlines(), output_file_content.splitlines()
-            )
-        )
+    output_file_content = pysparkler.upgrade(
+        input_file, output_file if not dry_run else None
     )
+
+    if ctx.obj["verbose"] or dry_run:
+        with open(input_file, encoding="utf-8") as f:
+            input_file_content = f.read()
+
+    if ctx.obj["verbose"]:
+        stdout.rule("Input")
+        stdout.print(Syntax(input_file_content, "python"))
+        stdout.rule("Output")
+        stdout.print(Syntax(output_file_content, "python"))
+
+    if dry_run:
+        stdout.rule("Unified Diff")
+        diff = difflib.unified_diff(
+            input_file_content.splitlines(), output_file_content.splitlines()
+        )
+        for line in diff:
+            stdout.print(Syntax(line, "python"))
+        stdout.rule("End of Diff")
+
+
+def print_command_params(ctx):
+    """Prints the command params"""
+    if ctx.obj["verbose"]:
+        table = Table(title="PySparkler CLI Parameters")
+        table.add_column("Param")
+        table.add_column("Value")
+
+        for key, value in ctx.obj.items():
+            table.add_row(key, str(value))
+
+        for key, value in ctx.params.items():
+            table.add_row(key, str(value))
+
+        stdout.print(table)
+
+
+if __name__ == "__main__":
+    run()
