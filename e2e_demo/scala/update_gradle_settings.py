02ca046096f315370b1aa314f3bb1c261e64bce0
Holden Karau <holden@pigscanfly.ca>
2023-04-04 16:37:08 -0700

    Fix the project rule

--- e2e_demo/scala/update_gradle_settings.py
+++ e2e_demo/scala/update_gradle_settings.py
@@ -1,4 +1,4 @@
 import re,sys;
 
 print(
-    re.sub(r"rootProject.name\s*=\s*[\'\"](.*?)[\"\']", "rootProject.name = \"\\1-3\"", sys.stdin.read()))
+    re.sub(r"rootProject.name\s*=\s*[\'\"](.*?)(-3)?(_2.1[12])?[\"\']", "rootProject.name = \"\\1-3\\3\"", sys.stdin.read()))
