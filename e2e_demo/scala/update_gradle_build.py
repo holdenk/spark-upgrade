aeae7fc46cad57c671c0082c5b8ebc9815eb0107
Holden Karau <holden@pigscanfly.ca>
2023-05-08 16:25:56 -0700

    Resolve version for gradle too.

--- e2e_demo/scala/update_gradle_build.py
+++ e2e_demo/scala/update_gradle_build.py
@@ -1,8 +1,10 @@
-import re,sys;
+import re,sys,os;
 
 original_build = sys.stdin.read()
 
 build_with_plugin = original_build
+
+version = os.getenv("SCALAFIX_RULES_VERSION", "0.1.14")
 
 if "scalafix" not in build_with_plugin:
     build_with_plugin = re.sub(
@@ -13,7 +15,7 @@ if "scalafix" not in build_with_plugin:
 
 build_with_plugin_and_rules = re.sub(
     r"dependencies\s*{",
-    "dependencies {\n    scalafix group: 'com.holdenkarau', name: 'spark-scalafix-rules-2.4.8_2.12', version: '0.1.9'\n",
+    f"dependencies {\n    scalafix group: 'com.holdenkarau', name: 'spark-scalafix-rules-2.4.8_2.12', version: '{version}'\n",
     build_with_plugin)
 
 print(build_with_plugin_and_rules)
