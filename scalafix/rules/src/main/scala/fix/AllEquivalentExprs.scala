459793739f247c703fffe1734e949bf02f806e13
Holden Karau <holden@pigscanfly.ca>
2023-05-09 15:05:01 -0700

    Add rule to migrate common subexprs checking (limited use but >1)

---
+++ scalafix/rules/src/main/scala/fix/AllEquivalentExprs.scala
@@ -0,0 +1,28 @@
+package fix
+
+import scalafix.v1._
+import scala.meta._
+
+class AllEquivalentExprs extends SemanticRule("AllEquivalentExprs") {
+
+  override def fix(implicit doc: SemanticDocument): Patch = {
+    val equivExprs = SymbolMatcher.normalized("org/apache/spark/sql/catalyst/expressions/EquivalentExpressions#getAllEquivalentExprs().")
+    val utils = new Utils()
+
+    def matchOnTree(e: Tree): Patch = {
+      e match {
+        case equivExprs(call) =>
+          // This is sketch because were messing with the string repr but it's easier
+          // since we only want to replace some of our match.
+          val newCall = call.toString.replace(".getAllEquivalentExprs", ".getCommonSubexpressions.map(List(_))")
+          Patch.replaceTree(call, newCall)
+        case elem @ _ =>
+          elem.children match {
+            case Nil => Patch.empty
+            case _ => elem.children.map(matchOnTree).asPatch
+          }
+      }
+    }
+    matchOnTree(doc.tree)
+  }
+}
