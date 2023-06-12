f4d010fb37e833eee87ac163838d1384a9432c56
Dhruv Pratap <dpratap@netflix.com>
2023-06-12 15:20:48 -0400

    PySparkler: Recommend using DataFrameWriter V2 APIs to save and write to tables when DataFrameWriter V1 API usage is detected.

--- pysparkler/pysparkler/pyspark_23_to_24.py
+++ pysparkler/pysparkler/pyspark_23_to_24.py
@@ -44,6 +44,60 @@ Arrow optimization is unable to be used. This can be switched off by spark.sql.e
             self.match_found = True
 
 
+class RecommendDataFrameWriterV2ApiForV1ApiSaveAsTable(StatementLineCommentWriter):
+    """Spark 2.4 introduced the new DataFrameWriterV2 API for writing to tables using data frames. The v2 API is
+    recommended for several reasons:
+        CTAS, RTAS, and overwrite by filter are supported
+        Hidden partition expressions are supported in partitionedBy
+    """
+
+    def __init__(
+        self,
+        pyspark_version: str = "2.4",
+    ):
+        super().__init__(
+            transformer_id="PY23-24-002",
+            comment=f"""As of PySpark {pyspark_version} the new DataFrameWriterV2 API is recommended for creating or \
+replacing tables using data frames. To run a CTAS or RTAS, use create(), replace(), or createOrReplace() operations. \
+For example: df.writeTo("prod.db.table").partitionedBy("dateint").createOrReplace(). Please note that the v1 DataFrame \
+write API is still supported, but is not recommended.""",
+        )
+
+    def visit_Call(self, node: cst.Call) -> None:
+        """Check if toPandas is being called"""
+        if m.matches(node, m.Call(func=m.Attribute(attr=m.Name("saveAsTable")))):
+            self.match_found = True
+
+
+class RecommendDataFrameWriterV2ApiForV1ApiInsertInto(StatementLineCommentWriter):
+    """Spark 2.4 introduced the new DataFrameWriterV2 API for writing to tables using data frames. The v2 API is
+    recommended for several reasons:
+        All operations consistently write columns to a table by name
+        Overwrite behavior is explicit, either dynamic or by a user-supplied filter
+    """
+
+    def __init__(
+        self,
+        pyspark_version: str = "2.4",
+    ):
+        super().__init__(
+            transformer_id="PY23-24-003",
+            comment=f"""As of PySpark {pyspark_version} the new DataFrameWriterV2 API is recommended for writing into \
+tables in append or overwrite mode. For example, to append use df.writeTo(t).append() and to overwrite partitions \
+dynamically use df.writeTo(t).overwritePartitions() Please note that the v1 DataFrame write API is still supported, \
+but is not recommended.""",
+        )
+
+    def visit_Call(self, node: cst.Call) -> None:
+        """Check if toPandas is being called"""
+        if m.matches(node, m.Call(func=m.Attribute(attr=m.Name("insertInto")))):
+            self.match_found = True
+
+
 def pyspark_23_to_24_transformers() -> list[cst.CSTTransformer]:
     """Return a list of transformers for PySpark 2.3 to 2.4 migration guide"""
-    return [ToPandasAllowsFallbackOnArrowOptimization()]
+    return [
+        ToPandasAllowsFallbackOnArrowOptimization(),
+        RecommendDataFrameWriterV2ApiForV1ApiSaveAsTable(),
+        RecommendDataFrameWriterV2ApiForV1ApiInsertInto(),
+    ]
--- pysparkler/tests/test_pyspark_23_to_24.py
+++ pysparkler/tests/test_pyspark_23_to_24.py
@@ -15,7 +15,11 @@
 #  specific language governing permissions and limitations
 #  under the License.
 #
-from pysparkler.pyspark_23_to_24 import ToPandasAllowsFallbackOnArrowOptimization
+from pysparkler.pyspark_23_to_24 import (
+    RecommendDataFrameWriterV2ApiForV1ApiInsertInto,
+    RecommendDataFrameWriterV2ApiForV1ApiSaveAsTable,
+    ToPandasAllowsFallbackOnArrowOptimization,
+)
 from tests.conftest import rewrite
 
 
@@ -50,5 +54,75 @@ pysparkDF = spark.createDataFrame(data = data, schema = columns)
 
 pandasDF = pysparkDF.toPandas()  # PY23-24-001: As of PySpark 2.4 toPandas() allows fallback to non-optimization by default when Arrow optimization is unable to be used. This can be switched off by spark.sql.execution.arrow.fallback.enabled  # noqa: E501
 print(pandasDF)
+"""
+    assert modified_code == expected_code
+
+
+def test_writes_comment_when_data_frame_writer_v1_api_save_as_table_is_detected():
+    given_code = """\
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('example').getOrCreate()
+
+data = [("James","","Smith","36636","M",60000),
+        ("Jen","Mary","Brown","","F",0)]
+
+columns = ["first_name","middle_name","last_name","dob","gender","salary"]
+pysparkDF = spark.createDataFrame(data = data, schema = columns)
+
+pysparkDF.write.partitionBy('gender').saveAsTable("persons")
+"""
+    modified_code = rewrite(
+        given_code, RecommendDataFrameWriterV2ApiForV1ApiSaveAsTable()
+    )
+    expected_code = """\
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('example').getOrCreate()
+
+data = [("James","","Smith","36636","M",60000),
+        ("Jen","Mary","Brown","","F",0)]
+
+columns = ["first_name","middle_name","last_name","dob","gender","salary"]
+pysparkDF = spark.createDataFrame(data = data, schema = columns)
+
+pysparkDF.write.partitionBy('gender').saveAsTable("persons")  # PY23-24-002: As of PySpark 2.4 the new DataFrameWriterV2 API is recommended for creating or replacing tables using data frames. To run a CTAS or RTAS, use create(), replace(), or createOrReplace() operations. For example: df.writeTo("prod.db.table").partitionedBy("dateint").createOrReplace(). Please note that the v1 DataFrame write API is still supported, but is not recommended.  # noqa: E501
+"""
+    assert modified_code == expected_code
+
+
+def test_writes_comment_when_data_frame_writer_v1_api_insert_into_is_detected():
+    given_code = """\
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('example').getOrCreate()
+
+data = [("James","","Smith","36636","M",60000),
+        ("Jen","Mary","Brown","","F",0)]
+
+columns = ["first_name","middle_name","last_name","dob","gender","salary"]
+pysparkDF = spark.createDataFrame(data = data, schema = columns)
+
+pysparkDF.write.insertInto("persons", overwrite=True)
+"""
+    modified_code = rewrite(
+        given_code, RecommendDataFrameWriterV2ApiForV1ApiInsertInto()
+    )
+    expected_code = """\
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('example').getOrCreate()
+
+data = [("James","","Smith","36636","M",60000),
+        ("Jen","Mary","Brown","","F",0)]
+
+columns = ["first_name","middle_name","last_name","dob","gender","salary"]
+pysparkDF = spark.createDataFrame(data = data, schema = columns)
+
+pysparkDF.write.insertInto("persons", overwrite=True)  # PY23-24-003: As of PySpark 2.4 the new DataFrameWriterV2 API is recommended for writing into tables in append or overwrite mode. For example, to append use df.writeTo(t).append() and to overwrite partitions dynamically use df.writeTo(t).overwritePartitions() Please note that the v1 DataFrame write API is still supported, but is not recommended.  # noqa: E501
 """
     assert modified_code == expected_code
